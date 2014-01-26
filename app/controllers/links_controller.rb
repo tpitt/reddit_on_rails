6de25fafa4926788e5060db8a5266e789744b60c
Tyson Pitt <tysonpitt@gmail.com>
2014-01-26 18:14:33 -0500

    End of part 1

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,20 @@
+class LinksController < ApplicationController
+
+	def show
+		@link = Link.find(params[:id])
+	end
+
+	def new
+		@link = Link.new
+	end
+
+	def create
+		@link = Link.new(params[:link].permit(:title, :url))
+		if @link.save
+			redirect_to @link
+		else
+			render 'new'
+		end
+	end
+
+end
